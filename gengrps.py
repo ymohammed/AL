0bf15a44ff9ed05ab01f3f10790000dbcaa314fb
ymohammed <yasin_mohammed@yahoo.com>
2017-05-13 12:21:57 -0700

    Add files via upload

---
+++ gengrps.py
@@ -0,0 +1,51 @@
+import random
+import pyodbc
+
+def grpsize(empn):
+    for x in range(3,6):
+        groups=0
+        if ((empn%x > 2) and (empn%x <6)) or empn%x==0:
+            groups=(empn/x)
+            break  
+    return empn/groups
+
+conn = pyodbc.connect(
+    r'DRIVER={ODBC Driver 13 for SQL Server};'
+    r'SERVER=localhost;'
+    r'DATABASE=test;'
+    r'Trusted_Connection=yes;'
+    )
+cursor=conn.cursor()
+cursor.execute("select * from employee")
+empn=[]
+for row in cursor:
+    emp_no=row[0]
+    empn.append(emp_no)
+
+#groupsize=grpsize(len(empn))
+groupno=0
+
+while len(empn)>0:
+    groupno+=1
+    grplist=[]
+    groupsize=random.randint(3,5)
+    if len(empn)<groupsize or len(empn) < 10:
+        if len(empn)>5:
+            groupcnt=(len(empn))-3
+        else:
+            groupcnt=len(empn)
+    else:
+        groupcnt=groupsize
+    while groupcnt>0:
+        if len(empn)>0:
+            x=random.choice(empn)
+            grplist.append(x)
+            empn.remove(x)
+            groupcnt-=1
+    grpsql='select emp_id, emp_name from employee where emp_id in (' + ','.join(map(str, grplist)) + ');'
+    print "\n----- Group %s ---------" %(groupno)
+    cursor.execute(grpsql)
+    for row in cursor:
+        print str(row[0]) + " " +row[1]
+
+
