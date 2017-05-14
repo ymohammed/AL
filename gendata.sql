87efae7cfebac61365fe922cb8c59680e77f7284
ymohammed <yasin_mohammed@yahoo.com>
2017-05-14 13:21:42 -0700

    Add files via upload

---
+++ gendata.sql
@@ -0,0 +1,65 @@
+-- create employee table
+create table employee(
+	emp_id int not null auto_increment,
+	emp_name varchar(50),
+	picked char(1)
+);
+
+-- Populate table with Mock Data
+
+insert into employee(emp_name,picked) values ('Yasin Mohammed','N');
+insert into employee(emp_name,picked) values ('Linda McArthy','N');
+insert into employee(emp_name,picked) values ('Susan Vasquez','N');
+insert into employee(emp_name,picked) values ('Bob Franklin','N');
+insert into employee(emp_name,picked) values ('John Keating','N');
+insert into employee(emp_name,picked) values ('Shirl Tace','N');
+insert into employee(emp_name,picked) values ('Orsola Bradmore','N');
+insert into employee(emp_name,picked) values ('Sheelah Longrigg','N');
+insert into employee(emp_name,picked) values ('Sheba Ogilby','N');
+insert into employee(emp_name,picked) values ('Buck Crammy','N');
+insert into employee(emp_name,picked) values ('Marilyn Stronge','N');
+insert into employee(emp_name,picked) values ('Courtenay Petrescu','N');
+insert into employee(emp_name,picked) values ('August Maine','N');
+insert into employee(emp_name,picked) values ('Melania Pagnin','N');
+insert into employee(emp_name,picked) values ('Marcellina Roundtree','N');
+insert into employee(emp_name,picked) values ('Karine Larrington','N');
+insert into employee(emp_name,picked) values ('Standford Armin','N');
+insert into employee(emp_name,picked) values ('See Bumpass','N');
+insert into employee(emp_name,picked) values ('Olive Ayris','N');
+insert into employee(emp_name,picked) values ('Pepito Bartram','N');
+insert into employee(emp_name,picked) values ('Belva Behnecken','N');
+insert into employee(emp_name,picked) values ('Laughton Tozer','N');
+insert into employee(emp_name,picked) values ('Leonidas Moyler','N');
+insert into employee(emp_name,picked) values ('Ulla Scadden','N');
+insert into employee(emp_name,picked) values ('Holden Harome','N');
+insert into employee(emp_name,picked) values ('Vicky McCowen','N');
+insert into employee(emp_name,picked) values ('Alica Jossum','N');
+insert into employee(emp_name,picked) values ('Eimile Grunnell','N');
+insert into employee(emp_name,picked) values ('Brew Studdert','N');
+insert into employee(emp_name,picked) values ('Peter Battman','N');
+insert into employee(emp_name,picked) values ('Baron McComish','N');
+insert into employee(emp_name,picked) values ('Aida Tummasutti','N');
+insert into employee(emp_name,picked) values ('Cathrine Andrioli','N');
+insert into employee(emp_name,picked) values ('Russ Hexham','N');
+insert into employee(emp_name,picked) values ('Breanne Burcombe','N');
+insert into employee(emp_name,picked) values ('Konstantin Rustich','N');
+insert into employee(emp_name,picked) values ('Jarid Janata','N');
+insert into employee(emp_name,picked) values ('Susy Le Floch','N');
+insert into employee(emp_name,picked) values ('Camellia Allner','N');
+insert into employee(emp_name,picked) values ('Angus Crippin','N');
+insert into employee(emp_name,picked) values ('Angelia Lomas','N');
+insert into employee(emp_name,picked) values ('Carena Gierth','N');
+insert into employee(emp_name,picked) values ('Dinny Found','N');
+insert into employee(emp_name,picked) values ('Laurena Spini','N');
+insert into employee(emp_name,picked) values ('Giacopo Cleaver','N');
+insert into employee(emp_name,picked) values ('Pepillo Feaster','N');
+insert into employee(emp_name,picked) values ('Ayn Vere','N');
+insert into employee(emp_name,picked) values ('Laurianne Milham','N');
+insert into employee(emp_name,picked) values ('Frayda Ferreras','N');
+insert into employee(emp_name,picked) values ('Leda Creak','N');
+insert into employee(emp_name,picked) values ('Mandi Walsh','N');
+insert into employee(emp_name,picked) values ('Cynthia Bisley','N');
+insert into employee(emp_name,picked) values ('Elie Dowtry','N');
+insert into employee(emp_name,picked) values ('Etan Rollett','N');
+insert into employee(emp_name,picked) values ('Rao Chiguru','N');
+insert into employee(emp_name,picked) values ('John Doe','N');
